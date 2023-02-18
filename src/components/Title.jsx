3ec0ad90e2f14ac413ceb9a0ebf9d9a0bedd0fca
herveyyy <captainhervey@gmail.com>
2023-02-18 16:59:01 +0800

    logo update

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 
 export const Title = () => {
   return (
-    <div >
+    <div>
+    <div className='w-20 mb-10 pt-6 '>
         <a href="https://liceo.edu.ph/"
         target="_blank"
-        rel="noopener noreferrer">
+        rel="noopener noreferrer"
+        className='to-blue-600'>
+          
         <img src="https://upload.wikimedia.org/wikipedia/en/1/18/Ldcu_seal.png"
-         className="w-20 mb-10 pt-5"/>
+         />
         </a>
     
+    </div>
     <h1 className="text-4xl md:text-7xl dark:text-white md:mb-3 font-bold text-center mb-8">Registration Form</h1>
     <div className='pt-12'></div>
     </div>
