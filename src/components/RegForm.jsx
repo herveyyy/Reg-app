31dcc8b2a514d62b4d087b4cb7d73804f01df3f4
herveyyy <captainhervey@gmail.com>
2023-02-18 18:08:41 +0800

    LiceanCaptcha

--- src/components/RegForm.jsx
+++ src/components/RegForm.jsx
@@ -8,7 +8,7 @@ import {
 } from 'firebase/firestore';
 import { useState } from 'react';
 import { database } from '../../firebaseConfig';
-
+import  Recaptcha  from 'react-google-recaptcha';
 
 export default function RegForm(){   
     const [firstName, setFirstName] = useState('');
@@ -18,7 +18,20 @@ export default function RegForm(){
     const [age, setAge] = useState('');
     const [address, setAddress] = useState('');
     const databaseReference = collection(database, 'Beneficiaries');
-    
+    const [captcha, setCaptcha] = useState('');
+
+    const handleCaptchaChange = (value) => {
+        setCaptcha(value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!captcha) {
+            alert('Please verify that you are a human!');
+            return;
+        }
+        addAndClear   
+     }
     const checkIfExist = async () => {
         console.log("Checking if data exists...");
     
@@ -63,6 +76,8 @@ export default function RegForm(){
     const addAndClear = () =>{
 
         if(firstName == "" && middleName == "" && lastName == "" && number == "" && age == "" && address == ""){
+         
+           
             alert("Please fill up all information")
             setFirstName("");
             setMiddleName("");
@@ -91,6 +106,7 @@ export default function RegForm(){
 
     return(
 <form>
+
 <div 
   className="grid md:grid-cols-3 md:gap-6">
     <div 
@@ -196,10 +212,15 @@ export default function RegForm(){
       htmlFor="floating_address" 
       className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
     Complete address</label>
+    
   </div>
+  <Recaptcha
+                sitekey="6LfUqJEkAAAAAM-dMwTcbgL5-KGagz2ANvQc93-b"
+                onChange={handleCaptchaChange}
+            />
   <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"
   type="button"
-onClick={addAndClear}
+onClick={handleSubmit}
   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
   </button>
   
